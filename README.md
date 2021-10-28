bc3d41d9d6549584f7cdee5a1bb04c4310d9b9f1
custompro98 <mitchjoa@gmail.com>
2021-10-28 17:07:03 -0400

    Base layout, integrate eslint/prettier

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # hdhh.club
- The official website of Hump Day Happy Hour 
+
+The official website of Hump Day Happy Hour
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
---
+++ components/layout.jsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Organize and randomize hump day happy hour"
+        />
+        <title>HDHH</title>
+      </Head>
+      <header className={styles.header}>
+        <Image
+          priority
+          src="/images/camel.svg"
+          height={144}
+          width={144}
+          alt="camel"
+        />
+        <h1 className={utilStyles.heading2Xl}>Hump Day Happy Hour</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
